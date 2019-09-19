40df2704747df3f2990efb746720437514d98c06
Martin Biscocho <32799798+martinibiscotti@users.noreply.github.com>
2019-09-19 12:10:16 -0700

    added admin restriction to !add

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
-from discord.ext import commands
+from discord.ext.commands import Bot, has_permissions, CheckFailure
 from benny import quotes
 
-bot = commands.Bot(command_prefix = '!')
+bot = Bot(command_prefix = '!')
 bot.remove_command('help')
 
 benny = quotes()
@@ -19,16 +19,22 @@ async def quote(ctx):
     await ctx.send(benny.quotes[0])
 
 @bot.command()
+@has_permissions(administrator=True)
 async def add(ctx, *newquote):
     benny.add(newquote)
     benny.load()
     await ctx.send('new quote added')
 
+@add.error
+async def add_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        await ctx.send('you do not have permission to add quotes')
+
 @bot.command()
 async def info(ctx):
     embed = discord.Embed(title = 'b3nnyb0t', description = 'bot that performs benny-related functions', color = 0xeee657)
     embed.add_field(name = 'github', value = '[repo](https://github.com/martinibiscotti/b3nnyb0t)')
-    embed.add_field(name = 'invite', value = '[link](https://discordapp.com/oauth2/authorize?client_id=489525731941023746&scope=bot)')
+    embed.add_field(name = 'invite', value = '[link](https://discordapp.com/oauth2/authorize?client_id=id_placeholder&scope=bot)')
     await ctx.send(embed = embed)
 
 @bot.command()
@@ -38,4 +44,4 @@ async def help(ctx):
     embed.add_field(name = '!quote', value = 'sends benny quote', inline = False)
     await ctx.send(embed = embed)
 
-bot.run('token')
+bot.run('token placeholder')
